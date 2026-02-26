77103c428e6e7b321f9ec45b4178a2da43082f09
jadedrb <jadecdrb@gmail.com>
2026-02-26 11:37:18 -0500

    added to the products page

--- mod-10/react-routes/src/pages/ProductsPage.jsx
+++ mod-10/react-routes/src/pages/ProductsPage.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom"
+
+const products = [
+    { id: 1, name: 'Laptop' },
+    { id: 2, name: 'TV' },
+    { id: 3, name: 'Radio' }
+]
+
 function ProductsPage() {
     return (
         <div>
             ProductsPage
+            <ul>
+                {products.map(product => 
+                    <Link key={product.id} to={"/products/" + product.id}>
+                        <li>{product.name}</li>
+                    </Link>
+                )}
+            </ul>
         </div>
     )
 }
