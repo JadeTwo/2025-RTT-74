471d0d60bf2b84c2967c84e3fb1b68bd2d0d1c2a
jadedrb <jadecdrb@gmail.com>
2026-02-26 13:37:31 -0500

    added a fetch request to the details page

--- mod-10/react-routes/src/pages/ProductDetailPage.jsx
+++ mod-10/react-routes/src/pages/ProductDetailPage.jsx
@@ -1,19 +1,34 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
-import { products } from "../data"
+// import { products } from "../data"
 
 function ProductDetailPage() {
 
+    const [product, setProduct] = useState({})
+
     const { id } = useParams()
+
+    useEffect(() => {
+
+        const getProduct = async () => {
+            const response = await fetch('https://dummyjson.com/products/' + id)
+            const data = await response.json()
+            setProduct(data)
+        }
+
+        getProduct()
+
+    }, [id])
     
-    const product = products.find(prod => prod.id == id)
+    // const product = products.find(prod => prod.id == id)
 
     return (
         <div>
             <h2>ProductDetailPage</h2>
             <div>
                 <div>Details for Product: {id}</div>
-                <div>Name: {product.name}</div>
+                <div>Name: {product.title}</div>
             </div>
             
         </div>
