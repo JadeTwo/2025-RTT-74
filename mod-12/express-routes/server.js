c40aaae2feab2750d5e1d84a6631445e643268ea
jadedrb <jadecdrb@gmail.com>
2026-03-05 10:45:44 -0500

    query strings

--- mod-12/express-routes/server.js
+++ mod-12/express-routes/server.js
@@ -12,6 +12,14 @@ app.get('/users/first/:firstName/last/:lastName', (req, res) => {
     res.send(`Welcome ${firstName} ${lastName}!`)
 })
 
+// Query Strings
+app.get('/pizzas', (req, res) => {
+    // access option params
+    const { size, toppings } = req.query
+    console.log(size, toppings)
+    res.send(`Pizza: ${size} ${toppings}`)
+})
+
 const port = 3000
 
 app.listen(port, () => {
